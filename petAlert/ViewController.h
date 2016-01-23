1b96abeb96bca2621c2059e9310ab602436b308a
dsmith510 <d.smith510@yahoo.com>
2016-01-22 16:48:57 -0800

    Initial Commit

---
+++ petAlert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  petAlert
+//
+//  Created by Danielle Smith on 1/22/16.
+//  Copyright © 2016 Danielle Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ petAlert/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  petAlert
+//
+//  Created by Danielle Smith on 1/22/16.
+//  Copyright © 2016 Danielle Smith. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
